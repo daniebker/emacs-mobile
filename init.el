7e47b119cca15109fcb1659f2c3ece95cd4fbbb5
Daniel Baker <bakerdude@gmail.com>
2019-03-28 08:25:40 +0100

    init

---
+++ init.el
@@ -0,0 +1,93 @@
+;; Disable the splash screen (to enable it agin, replace the t with 0)
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(setq inhibit-splash-screen t)
+
+;; Enable transient mark mode
+(transient-mark-mode 1)
+
+;;;;Org mode configuration
+;; Enable Org mode
+(require 'org)
+;; Make Org mode work with files ending in .org
+;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+;; The above is the default in recent emacsen
+
+(setq org-todo-keywords
+      '((sequence "TODO" "NEXT" "IN-PROGRESS" "WAITING" "DONE")))
+
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+
+(setq org-refile-targets
+      '(("gtd.org" :maxlevel . 1)
+        ("done.org" :maxlevel . 1)
+	("someday.org" :maxlevel . 1)))
+
+(setq org-agenda-custom-commands 
+      '(("w" "WORK" tags-todo "WORK"
+         ((org-agenda-overriding-header "work")))
+	("l" "LUNCHTIME" tags-todo "LUNCHTIME"
+         ((org-agenda-overriding-header "Lunch")))
+	("h" "HOME" tags-todo "HOME"
+         ((org-agenda-overriding-header "Home")))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
+ '(custom-safe-themes
+   (quote
+    ("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(hl-todo-keyword-faces
+   (quote
+    (("TODO" . "#dc752f")
+     ("NEXT" . "#dc752f")
+     ("THEM" . "#2d9574")
+     ("PROG" . "#4f97d7")
+     ("OKAY" . "#4f97d7")
+     ("DONT" . "#f2241f")
+     ("FAIL" . "#f2241f")
+     ("DONE" . "#86dc2f")
+     ("NOTE" . "#b1951d")
+     ("KLUDGE" . "#b1951d")
+     ("HACK" . "#b1951d")
+     ("TEMP" . "#b1951d")
+     ("FIXME" . "#dc752f")
+     ("XXX" . "#dc752f")
+     ("XXXX" . "#dc752f")
+     ("???" . "#dc752f"))))
+ '(org-agenda-files (quote ("~/notes/diary/gtd.org")))
+ '(org-modules
+   (quote
+    (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
+ '(package-selected-packages (quote (dracula-theme spacemacs-theme ox-hugo)))
+ '(pdf-view-midnight-colors (quote ("#b2b2b2" . "#292b2e"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(with-eval-after-load 'ox
+  (require 'ox-hugo))
+
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(setq org-capture-templates
+      '(("F" "Food Log" entry
+        (file+datetree+prompt "~/notes/diary/food.org")
+                "* %?\n%t\n%^{Meal}p%^{Type}p"))
+	)
